8e6fd77a9310c14db95cac636466c2772919491e
Phuc Mai <mariamaihong1309@gmail.com>
2024-01-29 11:09:50 -0500

    Toaster

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -19,7 +19,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <AuthProvider>{children}</AuthProvider>
+        <AuthProvider>
+          <ToasterContext />
+          {children}
+        </AuthProvider>
       </body>
     </html>
   );
